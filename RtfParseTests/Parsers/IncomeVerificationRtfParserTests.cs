8c33716cf7c35db2775fd424e7bb227defccf0c0
Renny Fadoju <rennyvbirds@yahoo.com>
2020-04-07 16:42:05 +0100

    Add tests for addressee's name

--- RtfParseTests/Parsers/IncomeVerificationRtfParserTests.cs
+++ RtfParseTests/Parsers/IncomeVerificationRtfParserTests.cs
@@ -19,17 +19,26 @@ namespace RtfParseTests
             _testFixtureDirectory = new DirectoryInfo("./../../../ExampleLettersHtml");
         }
 
-        [TestCase(0, "60065142")]
-        [TestCase(1, "50314947")]
+        [TestCase(0, "Claim Number 60065142")]
+        [TestCase(1, "Claim Number 50314947")]
+        [TestCase(2, "Claim Number 60617734")]
         public void ExecuteReturnsCorrectTitleAndClaimNumber(int fileNo, string claimNumber)
         {
             var fileInfo = _testFixtureDirectory.GetFiles().ElementAt(fileNo);
+            
+            _parser.Execute(fileInfo).Title.Should().Be("BENEFIT INCOME VERIFICATION DOCUMENT");
+            _parser.Execute(fileInfo).ClaimNumber.Should().Be(claimNumber);
+        }
+        
+        [TestCase(0, "Mr Richard Bruno")]
+        [TestCase(1, "Mr Wentworth Blackstock")]
+        [TestCase(2, "Ms Kazi Ayesha")]
+        
+        public void ExecuteReturnsCorrectAddresseeName(int fileNo, string name)
+        {
+            var fileInfo = _testFixtureDirectory.GetFiles().ElementAt(fileNo);
 
-            _parser.Execute(fileInfo).Should().BeEquivalentTo(new IncomeVerificationTemplate
-            {
-                Title = "BENEFIT INCOME VERIFICATION DOCUMENT",
-                ClaimNumber = claimNumber
-            });
+            _parser.Execute(fileInfo).Name.Should().Be(name);
         }
     }
 }
