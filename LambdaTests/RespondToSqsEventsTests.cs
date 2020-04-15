eb82907188a23380e184d41e5b503802ba63f336
Renny Fadoju <rennyvbirds@yahoo.com>
2020-04-15 18:11:59 +0100

    Add empty files for listening for /responding to SQS events

---
+++ LambdaTests/RespondToSqsEventsTests.cs
@@ -0,0 +1,7 @@
+namespace LambdaTests
+{
+    public class RespondToSqsEventsTests
+    {
+        
+    }
+}
---
+++ Usecases/ListenForSqsEvents.cs
@@ -0,0 +1,9 @@
+using AwsDotnetCsharp.UsecaseInterfaces;
+using UseCases.GatewayInterfaces;
+
+namespace UseCases
+{
+    public class ListenForSqsEvents : IListenForSqsEvents
+    {
+    }
+}
