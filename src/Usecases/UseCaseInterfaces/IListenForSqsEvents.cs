d25bed9b21f038fcd2dd37e7e6c32963094c1c5c
Louise Swift <louise.swift@madetech.com>
2020-04-17 13:19:49 +0100

    Initial setup of listenForSqsEvents lambda

--- src/Usecases/UseCaseInterfaces/IListenForSqsEvents.cs
+++ src/Usecases/UseCaseInterfaces/IListenForSqsEvents.cs
@@ -1,7 +1,10 @@
+using Amazon.Lambda.SQSEvents;
+using System.Collections.Generic;
+
 namespace AwsDotnetCsharp.UsecaseInterfaces
 {
     public interface IListenForSqsEvents
     {
-        
+        List<string> Execute(SQSEvent sqsEvent);
     }
 }
