5ba1ced39653d38b9fb43fccfda2ebb317fc0443
elcorbs <emma@madetech.com>
2020-04-20 10:39:04 +0100

    Save records to db usecase

--- src/Usecases/SaveRecordsToLocalDatabase.cs
+++ src/Usecases/SaveRecordsToLocalDatabase.cs
@@ -1,7 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Usecases.Domain;
+using UseCases.GatewayInterfaces;
+
 namespace UseCases
 {
     public class SaveRecordsToLocalDatabase
     {
-        
+        private readonly ILocalDatabaseGateway _databaseGateway;
+
+        public SaveRecordsToLocalDatabase(ILocalDatabaseGateway databaseGateway)
+        {
+            _databaseGateway = databaseGateway;
+        }
+
+        public async Task<List<DocumentDetails>> Execute(List<DocumentDetails> documentsToSave)
+        {
+            foreach (var document in documentsToSave)
+            {
+                var timestamp = await _databaseGateway.SaveDocument(document);
+                document.SavedAt = timestamp;
+            }
+            return documentsToSave.ToList();
+        }
     }
 }
