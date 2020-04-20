6a8e50dd4f43f01a0489f3f491654d24601ff2c4
elcorbs <emma@madetech.com>
2020-04-20 07:48:35 +0100

    Finish Dynamo Gateway

--- src/Gateways/LocalDatabaseGateway.cs
+++ src/Gateways/LocalDatabaseGateway.cs
@@ -1,7 +1,8 @@
 using System;
-using System.Globalization;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
 using Usecases.Domain;
 
 namespace Gateways
@@ -17,16 +18,52 @@ namespace Gateways
 
         public async Task<string> SaveDocument(DocumentDetails newDocument)
         {
-            var documentItem = new Document
+            var currentTimestamp = CurrentUtcUnixTimestamp();
+            while (true)
+            {
+                var documentItem = ConstructDocument(newDocument, currentTimestamp);
+
+                try
+                {
+                    var putConfig = ConditionalOnTimestampUniqueness(currentTimestamp);
+                    await _documentsTable.PutItemAsync(documentItem, putConfig);
+                }
+                catch (ConditionalCheckFailedException)
+                {
+                    currentTimestamp = CurrentUtcUnixTimestamp();
+                    continue;
+                }
+
+                break;
+            }
+
+            return currentTimestamp;
+        }
+
+        private static Document ConstructDocument(DocumentDetails newDocument, string currentTimestamp)
+        {
+            return new Document
             {
                 ["DocumentId"] = newDocument.DocumentId,
                 ["DocumentCreator"] = newDocument.DocumentCreator,
-                ["InitialTimestamp"] = CurrentUtcUnixTimestamp(),
+                ["InitialTimestamp"] = currentTimestamp,
                 ["Status"] = "Waiting"
             };
-            await _documentsTable.PutItemAsync(documentItem);
+        }
 
-            return null;
+        private static PutItemOperationConfig ConditionalOnTimestampUniqueness(string currentTimestamp)
+        {
+            return new PutItemOperationConfig
+            {
+                ConditionalExpression = new Expression
+                {
+                    ExpressionStatement = "InitialTimestamp <> :t",
+                    ExpressionAttributeValues = new Dictionary<string, DynamoDBEntry>{
+                    {
+                        ":t", currentTimestamp
+                    }}
+                },
+            };
         }
 
         private static string CurrentUtcUnixTimestamp()
--- test/GatewayTests/LocalDatabaseGatewayTests.cs
+++ test/GatewayTests/LocalDatabaseGatewayTests.cs
@@ -1,9 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DocumentModel;
-using Amazon.DynamoDBv2.Model;
 using AutoFixture;
 using FluentAssertions;
 using Gateways;
@@ -27,17 +26,10 @@ namespace GatewayTests
         [Test]
         public async Task AddRecordForDocumentId_SavesARecordToTheDatabase_WithDocumentIdAndCreator()
         {
-            var newDocument = new DocumentDetails
-            {
-                DocumentId = _fixture.Create<string>(),
-                DocumentCreator = _fixture.Create<string>()
-            };
+            var newDocument = RandomDocumentDetails();
             await _dbGateway.SaveDocument(newDocument);
 
-            var scanFilter = new ScanFilter();
-
-            var search = DatabaseClient.DocumentTable.Scan(scanFilter);
-            var response = await search.GetRemainingAsync();
+            var response = await GetItemsFromDatabase();
 
             response
                 .Where(doc => doc["DocumentId"] == newDocument.DocumentId)
@@ -48,40 +40,93 @@ namespace GatewayTests
         [Test]
         public async Task AddRecordForDocumentId_SavesARecordToTheDatabase_WithCorrectTimestamp()
         {
-            var newDocument = new DocumentDetails
+            var newDocument = RandomDocumentDetails();
+            await _dbGateway.SaveDocument(newDocument);
+            var expectedTimestamp = Convert.ToInt32((DateTime.UtcNow - DateTime.UnixEpoch).TotalSeconds);
+
+            var response = await GetItemsFromDatabase();
+            var savedTimestamp = GetTimestampForDocumentId(response, newDocument);
+            savedTimestamp.Should().BeCloseTo(expectedTimestamp, 1);
+        }
+
+        [Test]
+        public async Task AddRecordForDocumentId_ReturnsTheTimestampCreated()
+        {
+            var newDocument = RandomDocumentDetails();
+            var response = await _dbGateway.SaveDocument(newDocument);
+            var currentTimestamp = GetCurrentTimestamp();
+
+            Convert.ToInt32(response).Should().BeCloseTo(currentTimestamp, 1);
+        }
+
+        [Test]
+        public async Task AddRecordForDocumentId_IfTwoRecordsAddedAtTheSameTime_NeitherIsOverwritten()
+        {
+            var currentTimestamp = GetCurrentTimestamp();
+
+            var document1 = RandomDocumentDetails();
+            await AddDocumentToDatabase(document1, currentTimestamp);
+
+            var document2 = RandomDocumentDetails();
+            await _dbGateway.SaveDocument(document2);
+
+            var savedItems = await GetItemsFromDatabase();
+
+            savedItems.Count(doc => doc["DocumentId"] == document1.DocumentId).Should().Be(1);
+            savedItems.Count(doc => doc["DocumentId"] == document2.DocumentId).Should().Be(1);
+        }
+
+        [Test]
+        public async Task AddRecordForDocumentId_IfTwoRecordsAddedAtTheSameTime_ReturnsTimestampFromRetry()
+        {
+            var currentTimestamp = GetCurrentTimestamp();
+
+            var document1 = RandomDocumentDetails();
+            await AddDocumentToDatabase(document1, currentTimestamp);
+
+            var document2 = RandomDocumentDetails();
+            var response = await _dbGateway.SaveDocument(document2);
+
+            Convert.ToInt32(response).Should().BeGreaterThan(currentTimestamp);
+        }
+
+        private static int GetCurrentTimestamp()
+        {
+            return Convert.ToInt32((DateTime.UtcNow - DateTime.UnixEpoch).TotalSeconds);
+        }
+
+        private DocumentDetails RandomDocumentDetails()
+        {
+            return new DocumentDetails
             {
                 DocumentId = _fixture.Create<string>(),
                 DocumentCreator = _fixture.Create<string>()
             };
-            await _dbGateway.SaveDocument(newDocument);
-            var currentTimestamp = Convert.ToInt32((DateTime.UtcNow - DateTime.UnixEpoch).TotalSeconds);
+        }
+
+        private async Task AddDocumentToDatabase(DocumentDetails document1, int currentTimestamp)
+        {
+            var documentItem = new Document
+            {
+                ["DocumentId"] = document1.DocumentId,
+                ["DocumentCreator"] = document1.DocumentCreator,
+                ["InitialTimestamp"] = currentTimestamp.ToString(),
+                ["Status"] = "Waiting"
+            };
+            await DatabaseClient.DocumentTable.PutItemAsync(documentItem);
+        }
+
+        private static int GetTimestampForDocumentId(List<Document> savedItems, DocumentDetails document)
+        {
+            return (int) savedItems.First(doc => doc["DocumentId"] == document.DocumentId)["InitialTimestamp"];
+        }
+
+        private async Task<List<Document>> GetItemsFromDatabase()
+        {
             var scanFilter = new ScanFilter();
-            scanFilter.AddCondition("DocumentId", ScanOperator.Equal, newDocument.DocumentId);
 
             var search = DatabaseClient.DocumentTable.Scan(scanFilter);
-            var response = await search.GetRemainingAsync();
-
-            var timestamp = (int) response.First(doc => doc["DocumentId"] == newDocument.DocumentId)["InitialTimestamp"];
-            timestamp.Should().BeCloseTo(currentTimestamp, 1);
-        }
-
-        [Ignore("In Progress")]
-        [Test]
-        public void AddRecordForDocumentId_ReturnsTheTimestampCreated()
-        {
-            var newDocument = new DocumentDetails
-            {
-                DocumentId = _fixture.Create<string>(),
-                DocumentCreator = _fixture.Create<string>()
-            };
-            var response = _dbGateway.SaveDocument(newDocument);
-        }
-
-        [Ignore("In Progress")]
-        [Test]
-        public void AddRecordForDocumentId_IfDatabaseReturnsAnError_TriesAgain()
-        {
-            
+            return await search.GetRemainingAsync();
         }
     }
 }
