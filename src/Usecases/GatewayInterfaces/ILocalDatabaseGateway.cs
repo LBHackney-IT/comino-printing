0195e6c6c263541c317c5536121cd8916840fb59
elcorbs <emma@madetech.com>
2020-04-21 16:42:03 +0100

    Usecase to get doc details and set to processing

--- src/Usecases/GatewayInterfaces/ILocalDatabaseGateway.cs
+++ src/Usecases/GatewayInterfaces/ILocalDatabaseGateway.cs
@@ -1,10 +1,14 @@
 using System.Threading.Tasks;
 using Usecases.Domain;
+using Usecases.Enums;
 
 namespace UseCases.GatewayInterfaces
 {
     public interface ILocalDatabaseGateway
     {
         Task<string> SaveDocument(DocumentDetails newDocument);
+
+        Task<DocumentDetails> GetRecordByTimeStamp(string currentTimestamp);
+        Task<DocumentDetails> RetrieveDocumentAndSetStatusToProcessing(string savedDocumentSavedAt, LetterStatusEnum newStatus);
     }
 }
