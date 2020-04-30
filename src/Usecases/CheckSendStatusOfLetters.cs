cff391df39bc7f3f3bfa53b4013d941ebc807fff
Ben Pirt <ben@pirt.co.uk>
2020-04-30 09:32:13 +0100

    Add a try catch when checking in notify

--- src/Usecases/CheckSendStatusOfLetters.cs
+++ src/Usecases/CheckSendStatusOfLetters.cs
@@ -3,6 +3,7 @@ using Amazon.Lambda.Core;
 using Boundary.UseCaseInterfaces;
 using Usecases.GatewayInterfaces;
 using UseCases.GatewayInterfaces;
+using Notify.Exceptions;
 
 namespace Usecases
 {
@@ -24,13 +25,17 @@ namespace Usecases
             var lettersToCheck = await _localDatabaseGateway.GetLettersWaitingForGovNotify();
             lettersToCheck.ForEach(async letter =>
             {
-                LambdaLogger.Log($"Checking status for letter {letter.Id}, doc no {letter.CominoDocumentNumber}");
-                var govNotifyResponse = _govNotifyGateway.GetStatusForLetter(letter.Id, letter.GovNotifyNotificationId);
-                var updateResponse = await _localDatabaseGateway.UpdateStatus(letter.Id, govNotifyResponse.Status);
-                LambdaLogger.Log($"Updated status in Local DB to {govNotifyResponse.Status}");
-                if (updateResponse.StatusUpdated)
-                {
-                    await _logger.LogMessage(letter.Id, $"Gov Notify status: {govNotifyResponse.Status.PrettierStatusName()}");
+                try{
+                    LambdaLogger.Log($"Checking status for letter {letter.Id}, doc no {letter.CominoDocumentNumber} Notify ID: {letter.GovNotifyNotificationId}");
+                    var govNotifyResponse = _govNotifyGateway.GetStatusForLetter(letter.Id, letter.GovNotifyNotificationId);
+                    var updateResponse = await _localDatabaseGateway.UpdateStatus(letter.Id, govNotifyResponse.Status);
+                    LambdaLogger.Log($"Updated status in Local DB to {govNotifyResponse.Status}");
+                    if (updateResponse.StatusUpdated)
+                    {
+                        _logger.LogMessage(letter.Id, $"Gov Notify status: {govNotifyResponse.Status.PrettierStatusName()}").Wait();
+                    }
+                }catch(NotifyClientException e){
+                    LambdaLogger.Log($"Error checking status of document. ({e.Message})");
                 }
             });
         }
