56a0a9c970e94b24e06569f8b5a66edf1b32563d
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2020-05-28 13:56:14 +0100

    enable Benefit ICL parser

--- src/Usecases/ParserLookup.cs
+++ src/Usecases/ParserLookup.cs
@@ -15,9 +15,8 @@ namespace UseCases
                     return new BlankBenefitsDetails();
                 case "HB/CTR Invite to Claim":
                     return new HBCTRInviteToClaim();
-                //disabled due to rendering issues
-                //case "Benefit ICL":
-                //    return new BenefitICL();
+                case "Benefit ICL":
+                    return new BenefitICL();
                 case "Review ICL":
                     return new ReviewICL();
                 case "Adverse Inference (BEN 999)":
