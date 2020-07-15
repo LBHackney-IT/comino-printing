fb2a49545d4a439d67712bab273ff54c53794e8b
Liudvikas Taluntis <liudvikast@gmail.com>
2020-07-15 15:15:52 +0100

    Fixed [council name] text size inconsistency next to logo.
    For some reason, even though, all conversion is done from the same RTF template, with static header (council name + logo + some more text), ...
    ... the council name text size (Hackney) would vary from template to template - from being a large font it needs to be, to being a small default browser font.
    
    To fix this, some regex magic had to be done, wrap up the header council name with specific id to fix it. Had to resort to regex, because the RTF to HTML converter script gives off absolutelly horrid HTML (like almost everything being a classless and id'less <font> or <p> tag), making it difficult to do anything with it.

--- src/Usecases/UntestedParsers/CouncilTax/CTaxEnquiryForm.cs
+++ src/Usecases/UntestedParsers/CouncilTax/CTaxEnquiryForm.cs
@@ -13,6 +13,15 @@ namespace Usecases.UntestedParsers
     {
         public LetterTemplate Execute(string html)
         {
+            // Wrap up the council name into p HTML node so it could be targetted.
+            var cnCases = new Regex(@"(?<=>)\s*Hackney\s*(?=<i>Rev)|(?<=>)\s*Hackney\s*(?=</font>)", RegexOptions.Singleline);
+
+            html = cnCases.Replace(
+                    html,
+                    @"<b id=""council-name"">Hackney </b>",
+                    1
+                );
+
             var documentNode = GetDocumentNode(html);
             var address = ParseAddressIntoLines(documentNode);
 
@@ -51,6 +60,7 @@ namespace Usecases.UntestedParsers
 
                 - Make pre-form legal info (header-table) scale with its child elements by the same proportion.
                 - Make letter header (#letter-header) scale with its child elements by the same proportion.
+                - Fix the [council name] text size (when issue is present).
                 - Override the general CSS for element (mm) positioning (adress, header-table) as the existence of letter header changes the layout
                   dramatically. New values were recalculated out of GovNotify letter template example just as the original general CSS was.
                 - Added the hard @page margins as they were not in general CSS, making some margins too small (after removing default margins), and some too large when comparing to 
@@ -65,7 +75,7 @@ namespace Usecases.UntestedParsers
             $@".header-table ~ p {{margin-block-start: 0; margin-block-end: 0; margin: 0}}
               .header-table + p {{margin-block-start: 1em; margin-block-end: 1em; margin: 0}}
               .header-right font.c6, .header-right font.c10 {{font-size: {(int)Math.Floor(10 * rightTableRatio)}pt}}
-              /*.header-right p {{margin-block-start: 0; margin-block-end: 0; margin: 0;}}*/
+              #letter-header b#council-name {{font-family: impact; font-size: {(int)Math.Floor(22 * headerRatio)}pt}}
               #letter-header > p {{margin-block-start: 0; margin-block-end: 0; margin: 0; font-size: 0}}
               #letter-header p font.c1, #letter-header p font.c3 {{font-size: {(int)Math.Floor(22 * headerRatio)}pt}}
               #letter-header p font.c2, #letter-header p font.c4, #letter-header p font.c5, #letter-header p font.c6 {{font-size: {(int)Math.Floor(11 * headerRatio)}pt}}
